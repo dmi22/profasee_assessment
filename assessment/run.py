96eca1543caa9d88b39a564114d9336e16365997
Dmitrii <default_aspx@mail.ru>
2022-06-06 19:11:41 +0300

    Assessment

--- assessment/run.py
+++ assessment/run.py
@@ -1,6 +1,125 @@
+import pandas as pd
+import json
+import sqlalchemy
+from fastapi import FastAPI
+
+CONFIG_FILE_NAME = "config.json"
+
+
 def run():
     print("Hello, Profasee!")
 
 
+def get_config():
+    with open(CONFIG_FILE_NAME) as json_file:
+        return json.load(json_file)
+
+
+def unify_phone_separator(df):
+    df["PhoneNumber"].replace(regex="\.", value="-", inplace=True)
+
+
+def strip_interests(df):
+    for c in df.columns:
+        if "Interest" in c:
+            df[c] = df[c].str.strip()
+
+
+def filter_no_interests(df):
+    df.dropna(how='all', subset=[c for c in df.columns if "Interest" in c], inplace=True)
+
+
+def open_sql_conn(sql_db_url):
+    engine = sqlalchemy.create_engine(sql_db_url)
+    return engine.connect()
+
+
+def load_to_db(df, con, target_table_name):
+    df.to_sql(target_table_name, con, if_exists='replace', index=False)
+
+
+def get_age_stat(con):
+    return dict(con.execute(sqlalchemy.text(
+        "select min(p.Age) as min_age, max(p.Age) as max_age, avg(p.Age) as avg_age from people p"
+    )).fetchone())
+
+
+def get_the_biggest_city(con):
+    return dict(con.execute(sqlalchemy.text(
+        "select p.City as city_most_people from people p group by p.City order by count(*) desc limit 1"
+    )).fetchone())
+
+
+def get_top_5_interests(con):
+    cur = con.execute(sqlalchemy.text("select u.interest from "
+                                      "(select p.Interest1 as interest from people p union all "
+                                      "select p.Interest2 from people p union all "
+                                      "select p.Interest3 from people p union all "
+                                      "select p.Interest4 from people p) u "
+                                      "where u.interest is not null "
+                                      "group by u.interest "
+                                      "order by count(*) desc "
+                                      "limit 5"))
+    i = 1
+    res = dict()
+    for r in cur:
+        res[str(i) + " of top interests"] = r[0]
+        i += 1
+    return res
+
+
+def get_data_stats(con):
+    return get_age_stat(con) | get_the_biggest_city(con) | get_top_5_interests(con)
+
+
+config = get_config()
+
 if __name__ == "__main__":
     run()
+
+    # Task 1
+    df = pd.read_csv(config["src_url"])
+    df.to_csv(config["raw_data_file_path"], index=False)
+    df.to_json(config["converted_data_file_path"], orient="records")
+
+    # Task 2
+    unify_phone_separator(df)
+    strip_interests(df)
+    filter_no_interests(df)
+
+    # Task 3
+    con = open_sql_conn(config["sql_db_url"])
+    load_to_db(pd.read_csv(config["src_url"]), con, config["target_table_name"])
+    con.close()
+
+    # Task 4
+    con = open_sql_conn(config["sql_db_url"])
+    print(get_data_stats(con))
+    con.close()
+
+# Task 5
+app = FastAPI()
+
+
+@app.get("/get-age-stat")
+def get_age_stat_api():
+    con = open_sql_conn(config["sql_db_url"])
+    res = get_age_stat(con)
+    con.close()
+    return res
+
+
+@app.get("/get-the-biggest-city")
+def get_the_biggest_city_api():
+    con = open_sql_conn(config["sql_db_url"])
+    res = get_the_biggest_city(con)
+    con.close()
+    return res
+
+
+@app.get("/get-top-5-interests")
+def get_top_5_interests_api():
+    con = open_sql_conn(config["sql_db_url"])
+    res = get_top_5_interests(con)
+    con.close()
+    return res
--- assessment/test.py
+++ assessment/test.py
@@ -1,4 +1,6 @@
 import sqlalchemy
+import run
+import pandas as pd
 
 # connect to the database
 engine = sqlalchemy.create_engine("mysql://datatest:alligator@database/datatestdb")
@@ -23,3 +25,29 @@ print("Found rows in database: ", len(rows))
 assert len(rows) == len(names)
 
 print("Test Successful")
+
+
+# Unit tests
+def test_unify_phone_separator():
+    INPUT_VALUE = "111.111.1111"
+    RESULT_VALUE = "111-111-1111"
+
+    df_mock = pd.DataFrame({"PhoneNumber": [INPUT_VALUE]})
+    run.unify_phone_separator(df_mock)
+    if df_mock["PhoneNumber"][0] != RESULT_VALUE:
+        raise Exception("test_unify_phone_separator failed")
+
+
+def test_strip_interests():
+    INPUT_VALUE = "  Some interest "
+    RESULT_VALUE = "Some interest"
+
+    df_mock = pd.DataFrame({"Interest1": [INPUT_VALUE]})
+    run.strip_interests(df_mock)
+    if df_mock["Interest1"][0] != RESULT_VALUE:
+        raise Exception("test_strip_interests failed")
+
+
+test_unify_phone_separator()
+test_strip_interests()
+print("Unit tests passed successfully")
